84830ff0e8113bf772229d68a2980ca76a4cb83a
dit000 <dit000@dit000.local>
2019-04-09 15:58:10 +0900

    소수체크

--- Primenumber/ViewController.swift
+++ Primenumber/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var pmumber: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btncheck(_ sender: Any) {
+        
+        
+        
+    }
+    
 }
 
